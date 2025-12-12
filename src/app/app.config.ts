0710340408cfe41d6f18e4dc657343c4f090c869
Sandeep Pula <sandeeppula@Mac-15532.lan>
2025-12-11 21:44:07 -0500

    firebase connection

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withHashLocation, withInMemoryScrolling } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { environment } from '../environments/environment';
 
 import { routes } from './app.routes';
+
+const app = initializeApp(environment.firebaseConfig);
+const analytics = getAnalytics(app);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(
       routes,
-      withHashLocation(),
       withInMemoryScrolling({
         scrollPositionRestoration: 'top',
       })
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyBsttzluLIAwRpLjqWZhLvGFZmRAWCJW-s",
+        authDomain: "utsavkalp.firebaseapp.com",
+        projectId: "utsavkalp",
+        storageBucket: "utsavkalp.firebasestorage.app",
+        messagingSenderId: "675607501628",
+        appId: "1:675607501628:web:27dd90a6f15077de091c39",
+        measurementId: "G-98PYQT5D5E"
+    }
+};
